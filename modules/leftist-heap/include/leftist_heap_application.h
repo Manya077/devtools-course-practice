6ddf6e38ca70b79300628f47bbe1a3f18999c7f7
Manya077 <m.a.tihomirova.1999@yandex.ru>
2020-05-31 10:20:21 +0300

    commit2

--- modules/leftist-heap/include/leftist_heap_application.h
+++ modules/leftist-heap/include/leftist_heap_application.h
@@ -9,10 +9,10 @@
 #include "include/leftist_heap.h"
 
 class LeftHeapApp {
-public:
+ public:
   LeftHeapApp() = default;
   std::string operator()(int argc, const char** argv);
-private:
+ private:
   std::string help(const char* appname, const char* message = "");
   bool validateNumberOfArguments(int argc, const char** argv);
   int parseToValue(std::string strval);
@@ -21,39 +21,43 @@ private:
 };
 
 class LHOperation {
-public:
+ public:
   explicit LHOperation(const int _argc) : argc(_argc) {}
   virtual ~LHOperation() = default;
   static LHOperation* makeOperation(std::string op);
   virtual std::string operator()(LeftistHeap* heap,
     const std::vector<int>& arg) = 0;
   int getArgc() { return argc; }
-private:
+ private:
   int argc;
 };
 
 class InsertOperation : public LHOperation {
-public:
+ public:
   InsertOperation() : LHOperation(2) {}
-  std::string operator()(LeftistHeap* heap, const std::vector<int>& arg) override;
+  std::string operator()(LeftistHeap* heap,
+    const std::vector<int>& arg) override;
 };
 
 class FindMinOperation : public LHOperation {
-public:
+ public:
   FindMinOperation() : LHOperation(1) {}
-  std::string operator()(LeftistHeap* heap, const std::vector<int>& arg) override;
+  std::string operator()(LeftistHeap* heap,
+    const std::vector<int>& arg) override;
 };
 
 class DeleteMinOperation : public LHOperation {
-public:
+ public:
   DeleteMinOperation() : LHOperation(1) {}
-  std::string operator()(LeftistHeap* heap, const std::vector<int>& arg) override;
+  std::string operator()(LeftistHeap* heap,
+    const std::vector<int>& arg) override;
 };
 
 class IsEmptyOperation : public LHOperation {
-public:
+ public:
   IsEmptyOperation() : LHOperation(1) {}
-  std::string operator()(LeftistHeap* heap, const std::vector<int>& arg) override;
+  std::string operator()(LeftistHeap* heap,
+    const std::vector<int>& arg) override;
 };
 
-#endif  // MODULES_LEFTIST_HEAP_INCLUDE_LEFTIST_HEAP_H_
+#endif  // MODULES_LEFTIST_HEAP_INCLUDE_LEFTIST_HEAP_APPLICATION_H_
